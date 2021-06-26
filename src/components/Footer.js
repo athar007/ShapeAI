915931ba48ad536f4a55e337e164835373bd9c88
athar007 <49077407+athar007@users.noreply.github.com>
2021-06-26 20:46:48 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer">
+                <p>Copyright of ShapeAI @ { new Date().getFullYear() }</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
